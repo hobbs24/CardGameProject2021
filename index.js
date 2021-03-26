d749f61f95cf5cfabf1e43b00be1fd15cd651736
06111 <06111@farnborough.ac.uk>
2021-03-26 12:04:36 +0000

    A new and improved version. Still needs a leaderboard though.

--- index.js
+++ index.js
@@ -60,16 +60,15 @@ let deck = [ //Declaring the Array of Records
         name: "Yellow 10", suit: "Yellow", value: "10",
     }
 ]
-let p1deck=[]
+let p1deck=[] //Pre-declares the player's decks and win count
 let p2deck=[]
 let p1wins=0
 let p2wins=0
-let logged_in = 0
-let username = 0
-let password = 0
-let p1name = 0
+let logged_in = 0 //Shows how many players have logged in
+let p1name = 0 //Stores the names of players 1 and 2 and the password for player 1
+let p1password = 0
 let p2name = 0
-let just_logged_in = false
+let just_logged_in = false //Used to show if a player just logged in or not
 //let authorised = false //Declares that the user hasn't authorised yet, preventing them from playing
 function pageLoad(){ //The first function to run. When the page loads, this function runs to allow input from the submit button for logging in
     document.getElementById("login").addEventListener("submit", processLogin) //If the submit button is clicked, the processLogin functions runs
@@ -77,39 +76,36 @@ function pageLoad(){ //The first function to run. When the page loads, this func
 function authorisation(){
     let authorised_users = [{name:'Yaeger024', pass:'aot'}, {name: 'Excalibur', pass:'evLA55'}] //Array of authorised users (each in their own record) have their username and password declared
     let index=0 //Sets index to 0 to cycle through all indexes of the authorised_users array
-    let temp_username=document.getElementById("username").value //Assigns user input from the username field to variable username
-    let temp_password=document.getElementById("password").value //Assigns user input from the password field to variable password
-    if((temp_username===username) && (temp_password===password)){
-        alert("You can't input the same name and password in for both players")
+    let username=document.getElementById("username").value //Assigns user input from the username field to variable username
+    let password=document.getElementById("password").value //Assigns user input from the password field to variable password
+    if((username===p1name) && (password===p1password)){ //Checks to see if player 2 is trying to input player 1's login
+        alert("You can't input the same name and password in for both players") //Outputs a warning and doesn't allow them to play the game
     }
-    else if((temp_username!==username) || (temp_password!==password)){
-        username=temp_username
-        password=temp_password
+    else if((username!==p1name) || (password!==p1password)){ //If player 2 doesn't input player 1's login, their login details get searched for
         for(let i=0;i<authorised_users.length;i++){ //For loop that runs until either the username and password match or all elements have been checked
             if((authorised_users[index].name===username) && (authorised_users[index].pass===password)){ //If statement checks to see if the inputted username=a stored username at index in the authorised_users array
                 logged_in+=1 //Adds one to variable 'logged_in' to say how many players have logged in
                 document.getElementById("login").reset() //Resets the username and password fields
                 alert("Player has successfully logged in.")
-                if(logged_in===1){
-                    p1name = username
-                    document.getElementById("p1name").innerHTML = p1name
-                    console.log(p1name)
-                    just_logged_in = true
+                if(logged_in===1){ //Checks to see if player 1 has logged in,
+                    p1name = username //Assigns inputted username to p1name
+                    p1password = password //Assigns inputted password to password
+                    document.getElementById("p1name").innerHTML = p1name //Outputs player 1's name to the html page
+                    just_logged_in = true //Sets variable to true so that a below if statement doesn't run when player 1 successfully logs in
                 }
                 if(logged_in===2) {
-                    p2name = username
-                    console.log(p2name)
-                    document.getElementById("p2name").innerHTML = p2name
-                    shuffle(deck)
+                    p2name = username //Assigns inputted username to variable
+                    document.getElementById("p2name").innerHTML = p2name //Outputs player 2's name to the html page
+                    shuffle(deck) //Once both players have logged in, the deck gets shuffled
                 }
-                break //Ends the if statement
+                break //Ends the for loop if a player has logged in
             }
             else{
                 index++ //If no username was found, the next index is checked
-                just_logged_in = false
+                just_logged_in = false //Sets variable to false so that the below variable can run if a player doesn't manage to login successfully
             }
         }
-        if(!just_logged_in&&logged_in<2){
+        if(!just_logged_in&&logged_in<2){ //Runs if at least one player hasn't logged in and there's been an attempt after player 1 logged in
             alert("Your credentials are wrong.") //User is alerted their credentials are wrong and can try to login again
         }
     }
@@ -117,11 +113,10 @@ function authorisation(){
 }
 function processLogin(){ //The function to ensure only authorised users can play
     event.preventDefault() //Prevents the submit button from submitting a form
-    if(logged_in<2){ //Once authorised, this if statement runs
-        authorisation(logged_in, username, password)
+    if(logged_in<2){ //If at least one player hasn't been authorised, this if statement runs
+        authorisation(logged_in) //Calls the authorisation function
     }
-    else if(logged_in===2){
-        alert("Logged in") //User is alerted that they've successfully logged in
+    else if(logged_in===2){ //If both players have logged in, this runs
         shuffle(deck) //Calls the shuffle function and runs it with the array 'deck'
     }
 }
@@ -135,7 +130,7 @@ function shuffle(deck) { //Runs the shuffle function with the array 'deck'
         deck[i] = deck[index] //Assigns a value to its new spot
         deck[index] = temp //Assigning the temporary variable to its new spot
     }
-    alert("Deck has been shuffled. You may now play") //Alerts the user they can actually play the game now
+    alert("Deck has been shuffled. You may now play.") //Alerts the user they can actually play the game now
     return(deck) //Returns deck to the function 'processLogin'
 }
 function pickup(){ //The pickup function is declared when the pickup button is clicked
@@ -154,11 +149,11 @@ function pickup(){ //The pickup function is declared when the pickup button is c
                     document.getElementById("winner-declaration").innerHTML = p1name //Displays that player 1 won that round
                     document.getElementById("winner-reason").innerHTML = 'Due to having a greater card value' //Displays the reason player 1 beat player 2
                 }
-                else{
-                    p2deck.push(p1card.name, p2card.name) //Runs if player 2's card has a higher value than player 1's
-                    p2wins++
+                else{//Runs if player 2's card has a higher value than player 1's
+                    p2deck.push(p1card.name, p2card.name)  //Adds both players' cards to player 2's deck (an array)
+                    p2wins++ //Adds one to player 2's wins
                     document.getElementById("winner-declaration").innerHTML = p2name
-                    document.getElementById("winner-reason").innerHTML = 'Due to having a greater card value'
+                    document.getElementById("winner-reason").innerHTML = 'Due to having a greater card value' //Displays the reason player 2 beat player 1
                 }
             }
             else if(p1card.suit==="Red"&&p2card.suit==="Black"){ //Checks to see if player 1 has the dominant card in this situation
@@ -201,15 +196,15 @@ function pickup(){ //The pickup function is declared when the pickup button is c
         else if((p1wins+p2wins)>=15){ //Runs once all the cards have been played
             alert("That is the end of the game") //Alerts the players that the game is over
             if(p1wins>p2wins){ //Checks to see if player 1 won the game overall
-                document.getElementById("deck_outputs").innerHTML = p1name+" wins. Here is their deck: "+JSON.stringify(p1deck) //Displays player 1's deck if they won
+                document.getElementById("deck_outputs").innerHTML = p1name+" wins with "+p1wins+" rounds won. Here is their deck: "+JSON.stringify(p1deck) //Displays player 1's deck if they won
             }
             else{
-                document.getElementById("deck_outputs").innerHTML = p2name+" wins. Here is their deck: "+JSON.stringify(p2deck) //Displays player 2's deck if they won
+                document.getElementById("deck_outputs").innerHTML = p2name+" wins with "+p2wins+" rounds won. Here is their deck: "+JSON.stringify(p2deck) //Displays player 2's deck if they won
             }
 
         }
     }
-    else{
+    else{ //If at least one of the players hasn't logged in, they will be told to if the click the button
         alert("You must login in both players before playing.")
     }
 }
